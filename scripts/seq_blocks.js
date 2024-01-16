7c9e721e9268bb3736aa262c27a14f3cd6ad733e
Jennifer Wang <38547472+jennjwang@users.noreply.github.com>
2024-01-15 20:13:04 -0500

    Update seq_blocks.js

--- scripts/seq_blocks.js
+++ scripts/seq_blocks.js
@@ -750,7 +750,7 @@ Blockly.JavaScript.PRECEDENCE = 0;
 Blockly.JavaScript["e_thing_not_in_room"] = function (block) {
   let value = block.getFieldValue("VALUE");
   if (value == "toy") {
-    return ["toy_not_in_room\t", Blockly.JavaScript.PRECEDENCE];
+    return ["toy_not_in_room()\t", Blockly.JavaScript.PRECEDENCE];
   } else if (value == "coffee") {
     return ["thing_not_in_room('coffee')\t", Blockly.JavaScript.PRECEDENCE];
   } else if (value == "mail") {
